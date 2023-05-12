879de30bdf2ab5a0d39b570ff73e8778fdcf4e61
Levy Araujo <levy4raujo@gmail.com>
2023-05-12 14:52:11 -0300

    [Req 01 Concluído]
    Listando todos ou um produto.

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const conn = require('./connection');
 
 const app = express();
+app.use(express.json());
 
 // não remova esse endpoint, é para o avaliador funcionar ( teste )
 app.get('/', (_request, response) => {
@@ -10,4 +12,21 @@ app.get('/', (_request, response) => {
 // não remova essa exportação, é para o avaliador funcionar
 // você pode registrar suas rotas normalmente, como o exemplo acima
 // você deve usar o arquivo index.js para executar sua aplicação 
+
+app.get('/products', async (_req, res) => {
+  const [result] = await conn.execute('SELECT * FROM StoreManager.products');
+  res.status(200).json(result);
+});
+
+app.get('/products/:id', async (req, res) => {
+  const { id } = req.params;
+  const [[result]] = await conn.execute(
+    'SELECT * FROM StoreManager.products WHERE id = ? ORDER BY id', [id]);
+  if (!result) {
+    res.status(404).json({ message: "Product not found" });  
+  } else {
+    res.status(200).json(result);  
+  }
+});
+
 module.exports = app;
